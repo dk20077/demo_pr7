6ff756e69371a56e2012366e742e814ad6dad945
dk20077 <sasthaplater1@gmail.com>
2026-03-17 22:29:49 +0530

    Add files via upload

---
+++ uc6.java
@@ -0,0 +1,60 @@
+public class OOPSBannerUC6 {
+
+    // Method for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*      ",
+            " ***** ",
+            "      *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        // Combine patterns line by line
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
+
+        // Print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
